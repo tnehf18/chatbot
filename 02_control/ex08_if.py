460b96520d892b41606eb7b60ba34ca49b49f2be
NOZ <71051811+tnehf18@users.noreply.github.com>
2021-01-19 17:44:48 +0900

    조건문 if

--- 02_control/ex08_if.py
+++ 02_control/ex08_if.py
@@ -1,39 +1,205 @@
 # 파이썬 기초 (+카카오톡 챗봇) 스터디 1주차
 
 # python 제어문
+# 1. 조건문 if
+
+# 1.1 기본 형태
+print("\n[ 1.1 기본 형태 ] ─────────────────────────────────────────────────────────────────────────────────────────\n")
 
 # 기본적으로 python 의 제어문은 C계열 언어에서 괄호나 중괄호가 쓰이는 것과 달리,
-# 띄어쓰기나 탭, 줄바꿈으로 이루어지므로, 이에 유의해야 한다.
+# 띄어쓰기나 탭, 줄바꿈 등 들여쓰기(indent)에 주의해서 작성해야 한다.
 
-# 1. if문
+a = 5
+b = 12
+if a > b:
+    print("a가 b 보다 큽니다.")
+else:
+    print("b가 a 보다 큽니다.")
+
 
 # 기본적으로 if ~ else 의 구조로, 참과 거짓의 조건에 따라 실행하는 형태.
 
-a = int(input("숫자 a를 입력하세요.\n"))
-b = int(input("숫자 b를 입력하세요.\n"))
+# C계열 언어나 Java 등의 다른 언어를 먼저 배운 사람들은 조금 적응하기 힘든 부분.
+# 개인적으로 들여쓰기에 너무 민감해서 굉장히 번거롭고, 가뜩이나 세미콜론(;)도 잘 안 쓰는데 너무 해괴하게 보임...
+# 또한, else 뒤에 콜론(:)을 반드시 붙여야 하는 점도 약간 짜증.
+
+# 리스트 활용시
+a = (1, 2, 3, 4)
+b = []
+if bool(b):
+    print("b 는 True 입니다.")
+else:
+    print("b 는 False 입니다.")
+
+
+# 굳이 bool 로 캐스팅하지 않아도 인식함.
+if []:
+    print("생성 되었습니다.")
+else:
+    print("아무것도 들어있지 않습니다.")
+
+
+# ※ javaScript 에서 빈 배열은 true 로 인식함으로 이를 조심.
+
+# if ([]) {
+#     alert("빈 배열이지만, true 입니다.");
+# }
+# else {
+#     alert("배열 내부는 비었고, false 입니다.");
+# }
+
+
+# 1.2 여러 조건문 실행
+print("\n[ 1.2 여러 조건문 실행 ] ──────────────────────────────────────────────────────────────────────────────────\n")
+
+# elif
+print("\n# elif")
+
+# if ~ else 만으로 할 경우
+
+a = 5
+b = 12
+c = 1
 
 if a > b:
-    print("\n" + str(a) + " > " + str(b))
-    print("a가 b보다 큽니다.")
+    print("a가 b 보다 큽니다.")
 else:
-    print("b가 a보다 큽니다.")
+    if a > c:
+        print("a는 b 보다 작고 c 보다 큽니다.")
+    else:
+        print("a가 가장 작습니다.")
 
-# elif 문
+# elif 사용시
 
-score = int(input("당신의 국어 성적은 몇 점입니까? "))
-
-if score > 90:
-    print("당신의 학점은 A 입니다.")
-elif score > 80:
-    print("당신의 학점은 B 입니다.")
-elif score > 70:
-    print("당신의 학점은 C 입니다.")
-elif score > 60:
-    print("당신의 학점은 D 입니다.")
+if a > b:
+    print("a가 b 보다 큽니다.")
+elif a > c:
+    print("a는 b 보다 작고 c 보다 큽니다.")
 else:
-    print("당신의 학점은 F 입니다.")
+    print("a가 가장 작습니다.")
 
-# 2. for, while 문
+# 한 줄로 표현
 
-# while 문   : 조건문과 많이 활용.
-# for 문     : list, set 등의 자료구조와 많이 활용.
+if a > b: print("a가 b 보다 큽니다.")
+elif a > c: print("a는 b 보다 작고 c 보다 큽니다.")
+else: print("a가 가장 작습니다.")
+
+# 다만 한 줄로 표현할 경우, 실행문이 길면 각 실행문을 구분하기 위해 세미콜론(;)이 필요해지고, 더 보기 안 어려워질 수 있음.
+
+if a > b: print("a가 b 보다 큽니다."); print("야호")
+else: print("a가 가장 작습니다.")
+
+
+# and, or, not
+print("\n# and, or, not")
+
+# 이렇게 쓰지 않도록 주의. (&, | 기호 보다 and, or, not 을 권장하는 이유)
+
+if True & True:
+    print("참입니다.")
+else:
+    print("거짓입니다.")
+
+
+if 100 & True:
+    print("참입니다.")
+else:
+    print("거짓입니다.")
+
+# 여기까지는 가능하나, 아래는 모두 에러가 발생.
+
+# if 1.1 & True:
+#     print("참입니다.")
+# else:
+#     print("거짓입니다.")
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# TypeError: unsupported operand type(s) for &: 'float' and 'bool'
+
+# if True & [1, 2, 3, 4]:
+#     print("참입니다.")
+# else:
+#     print("거짓입니다.")
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# TypeError: unsupported operand type(s) for &: 'bool' and 'list'
+
+# if True & "문자열":
+#     print("참입니다.")
+# else:
+#     print("거짓입니다.")
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# TypeError: unsupported operand type(s) for &: 'bool' and 'str'
+
+
+# 기본적으로 Python 에서 &, | 연산자는 교집합과 합집합 연산자로 동작한다.
+
+if True and "":
+    print("참입니다.")
+else:
+    print("거짓입니다.")
+
+
+if False or [1, 2, 3, 4]:
+    print("참입니다.")
+else:
+    print("거짓입니다.")
+
+
+if not "참":
+    print("참입니다.")
+else:
+    print("거짓입니다.")
+
+# pass : if 문에서 아무것도 실행하지 않고자 할 경우, 사용.
+
+pocket = ['paper', 'money', 'cellphone']
+
+if 'money' in pocket:
+    pass
+else:
+    print("money가 없습니다.")
+
+# if 'money' in pocket:
+# else:
+#     print("money가 없습니다.")
+
+# Java 나 C계열 언어에서는 실행문이 비어 있어도 그냥 넘어가지만, Python 에서는 에러가 발생하므로, pass 를 명시해야 한다.
+
+
+# 1.3 다른 표현 방법
+print("\n[ 1.3 다른 표현 방법 ] ────────────────────────────────────────────────────────────────────────────────────\n")
+
+# 조건부 표현식
+print("\n# 조건부 표현식")
+
+score = int(input("점수를 입력해주세요.\n"))
+
+msg = "success" if score >= 80 else "failure"
+print(score, msg)
+
+
+# is 키워드
+print("\n# is 키워드")
+
+# == 비교연산자와 비슷한 is 가 존재하지만, 주의.
+
+a = [1, 2, 3]
+b = a           # 삽입되는 값은 a 라는 객체 그 자체이며, 그 개체의 고유한 id 값이 들어감. -> 얕은 복사
+
+if a is b:
+    print("같습니다.")
+else:
+    print("다릅니다.")
+
+
+c = a.copy()    # a 객체의 값을 복사하여 새로운 객체 c 에 넣음. -> 깊은 복사
+
+if a is c:
+    print("같습니다.")
+else:
+    print("다릅니다.")
+
+# is 는 생성된 변수의 id 값이 같은지 비교함.
+# Python 에서 변수의 id 값은 내부 객체 및 생성된 변수를 가리키는 고유한 상수이지만, 메모리 주소는 아님.
