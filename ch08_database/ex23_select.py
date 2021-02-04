5d9379e2f6382266001b315028dfa8e3bdeb3e19
NOZ <71051811+tnehf18@users.noreply.github.com>
2021-02-04 11:14:14 +0900

    데이터 조회 select

--- ch08_database/ex23_select.py
+++ ch08_database/ex23_select.py
@@ -47,15 +47,53 @@ else:
 
     result = cursor.fetchall()      # 조회된 결과를 튜플로 반환함.
 
-    [print(x) for x in result]      # row 도 튜플로 나타남.
-
     print(f"[DB 수행 성공]: {db.host_info} {db.user.decode('utf-8')} [{dt.datetime.now()}]", sql)
     print(f"수행 결과 : {result.__len__()}")
+
+    [print(x) for x in result]      # row 도 튜플로 나타남.
 finally:
     cursor.close()
     db.close()
     print(f"[DB 접속 종료]: {db.host_info} {db.user.decode('utf-8')} [{dt.datetime.now()}]")
 
 
-# ☆☆☆ DictCursor
-print("\n[ ☆☆☆ DictCursor ] ────────────────────────────────────────────────────────────────────────────────────────\n")
+# ☆☆☆
+# 2.2.1 DictCursor
+print("\n[ 2.2.1 DictCursor ] ──────────────────────────────────────────────────────────────────────────────────────\n")
+
+# 딕셔너리 커서 자체는 모든 쿼리문에 사용 가능하지만, 사실상 select 문 외에 불필요.
+
+sql = '''
+SELECT * FROM tb_student WHERE age > %(age)s
+'''
+
+params = {"age": 30}
+
+# 재접속
+db.connect()
+
+cursor = db.cursor(pymysql.cursors.DictCursor)      # DictCursor 선언
+
+try:
+    cursor.execute(sql, params)
+except Exception as e:
+    cursor.rollback()
+    print(f"[SQL 에러]: {e.__traceback__}")
+else:
+
+    result = cursor.fetchall()      # 조회된 결과를 각 row 가 딕셔너리로 이루어진 리스트로 반환함.
+    print(result, type(result))
+
+    print(f"[DB 수행 성공]: {db.host_info} {db.user.decode('utf-8')} [{dt.datetime.now()}]", sql)
+    print(f"수행 결과 : {result.__len__()}")
+
+    print("슬픈 만 30살 이상 아재 목록: ")
+
+    # 각 row 가 딕셔너리 이므로 딕셔너리의 특성을 이용하여 특정 키(컬럼)의 값만 뽑아서 활용하기에 편리.
+    for x in result:
+        print(x["name"], x)
+finally:
+    cursor.close()
+    db.close()
+    print(f"[DB 접속 종료]: {db.host_info} {db.user.decode('utf-8')} [{dt.datetime.now()}]")
+
